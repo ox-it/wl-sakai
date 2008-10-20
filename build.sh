3f549741036f06f3e55b57845d797aba4e586a67
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2008-10-20 14:28:21 +0100

    Tidy up parameters and filenames.

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/sh -xv
 #
 # Short build script.
 
+local_version=2.5-ox2.0
+local_sakai=2.5.x
 
-mvn -Dmaven.test.skip=true -Dmaven.tomcat.home=${pwd}/build/ clean install sakai:deploy -Pfull,oxford
-(cd build; tar zcf ../tomcat.tgz .)
+MAVEN_OPTS="-Xms168m -Xmx512m -XX:PermSize=32m -XX:NewSize=64m -Dmaven.tomcat.home=`pwd`/build/ -Dmaven.test.skip=true -Dsakai.withjsmath=true"
+
+export MAVEN_OPTS
+
+rm -rf build
+mvn clean install sakai:deploy -Pfull,oxford -Dlocal.service=$local_version -Dlocal.sakai=$local_sakai
+(cd build; tar zcf ../sakai-${local_version}.tgz .)
