5fb8d2b3c097a3b3cceb7281783b6c33c4525ab2
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-02-19 10:59:12 +0000

    settings.xml overrides MAVEN_OPTS (causes issues) For important settings they should all be on the command line.

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 local_version=2.5-ox2.2
 local_sakai=2.5.x
 
-MAVEN_OPTS="-Xms168m -Xmx512m -XX:PermSize=96m -XX:NewSize=64m -Dmaven.tomcat.home=`pwd`/build/ -Dmaven.test.skip=true -Dsakai.withjsmath=true"
+MAVEN_OPTS="-Xms168m -Xmx512m -XX:PermSize=96m -XX:NewSize=64m -Dmaven.test.skip=true -Dsakai.withjsmath=true"
 
 export MAVEN_OPTS
 
 rm -rf build
-mvn clean install sakai:deploy -Pfull,oxford -Dlocal.service=$local_version -Dlocal.sakai=$local_sakai || exit 1
+mvn clean install sakai:deploy -Pfull,oxford -Dlocal.service=$local_version -Dlocal.sakai=$local_sakai -Dmaven.tomcat.home=$(pwd)/build/ || exit 1
 (cd build && tar zcf ../sakai-${local_version}.tgz .)
