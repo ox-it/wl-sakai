beb1378521382910377dc04787541f0ddde87b7a
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-03-13 16:34:58 +0000

    Final bugfix build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 #
 # Short build script.
 
-local_version=2.5-ox3.1-rc1
+local_version=2.5-ox3.1
 local_sakai=2.5.x
 
 MAVEN_OPTS="-Xms168m -Xmx512m -XX:PermSize=96m -XX:NewSize=64m -Dmaven.test.skip=true -Dsakai.withjsmath=true"
