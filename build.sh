2487b34e3c4519a462bb6807c25d1bf0d73344ba
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-04-23 10:09:49 +0100

    Make sure the submodules are up to date.

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@
 local_version=2.5-ox4
 local_sakai=2.5.x
 
+git submodule sync
+git submodule update
+
 MAVEN_OPTS="-Xms168m -Xmx512m -XX:PermSize=96m -XX:NewSize=64m -Dmaven.test.skip=true -Dsakai.withjsmath=true"
 
 export MAVEN_OPTS
